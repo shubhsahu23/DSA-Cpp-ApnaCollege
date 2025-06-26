e9cff99ca5f39e4230a2a363fb0c75a2cac64ae4
shubh <148770212+shubhsahu23@users.noreply.github.com>
2025-06-26 23:07:32 +0530

    Implementing data types

---
+++ Basics/data_types.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age=25;//4 bytes
+    char grade='a';//1 byte
+    float PI=3.14;//4 bytes
+    bool isSafe=false;//1 byte
+    double value=5.6965;//8 bytes
+    return 0;
+}
