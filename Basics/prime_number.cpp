4933e018f9a63ac115f5e27f368674e5d3e54ac4
shubh <148770212+shubhsahu23@users.noreply.github.com>
2025-06-26 23:23:21 +0530

    check prime number

---
+++ Basics/prime_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=7;
+    bool isPrime=true;
+    for(int i=2;i<=n-1;i++){
+        if(n%i==0){
+            isPrime=false;
+            break;
+        }
+    }
+    if(isPrime==true){
+        cout<<"prime";
+    }
+    else{
+        cout<<"not prime";
+    }
+    return 0;
+}
