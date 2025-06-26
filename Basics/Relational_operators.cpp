99bfc02be1c205723f24196b6c9be75cbbbf3bcf
shubh <148770212+shubhsahu23@users.noreply.github.com>
2025-06-26 23:12:51 +0530

    Implenting Relational Operators

---
+++ Basics/Relational_operators.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<(3<5)<<endl;//true->1
+    cout<<(3>5)<<endl;//false->1
+    cout<<(3<=3)<<endl;//true->1
+    cout<<(3!=5)<<endl;//true->1
+    cout<<(3==5)<<endl;//false->0
+    return 0;
+}
