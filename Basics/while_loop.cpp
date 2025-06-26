69c821468aafda31dae4df552419bdac6581ef5a
shubh <148770212+shubhsahu23@users.noreply.github.com>
2025-06-26 23:19:35 +0530

    Use of while loop

---
+++ Basics/while_loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //print numbers 1 to 5
+    int count =1;
+    while(count<=5){
+        cout<<count<<endl;
+        count++;
+
+    }
+    return 0;
+}
