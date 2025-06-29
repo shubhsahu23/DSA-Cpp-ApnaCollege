e04b0c09d03c30529920d82b6f7dd82f76879d8d
shubh <148770212+shubhsahu23@users.noreply.github.com>
2025-06-29 15:43:35 +0530

    Implementing bitwise operators

---
+++ Basics/bitwiseOperators.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=8,b=5;
+    cout<<(a & b)<<endl;//bitwise and operator
+    cout<<(a | b)<<endl;//bitwise or operator
+    cout<<(a ^ b)<<endl;//bitwise xor operator
+    cout<<(4<<1)<<endl;//right shift operator
+    cout<<(4>>1)<<endl;//left shift operator
+    return 0;
+}
