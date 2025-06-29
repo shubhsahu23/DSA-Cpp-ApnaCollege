838bf2c2bb42e254b32d4c6f66fff949c801620c
shubh <148770212+shubhsahu23@users.noreply.github.com>
2025-06-30 00:09:29 +0530

    Reverse the given array

---
+++ Arrays/reverseAnArray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void reverseArray(int arr[],int size){
+    int start=0,end=size-1;
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+
+    }
+}
+int main(){
+    int arr[]={4,2,7,8,1,2,5};
+    int size=7;
+    reverseArray(arr,size);//time complexity O(n)
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
